ff8c47070042c802c13a2f20b0c625e3ebd0c453
Joaquinossandon <joaquin.ossandon@inacapmail.cl>
2025-02-06 20:15:46 -0300

    add useEffect to fetch and log time data from external API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const consultaHora = async () => {
+      const response = await fetch('https://server-test-black.vercel.app/hora')
+      const data = await response.json()
+      console.log(data)
+    }
+
+    consultaHora()
+  }, [])
 
   return (
     <>
